49e941aedbea60b7317ff5aeaed18253225e8d58
Rahul C V <rahulcv447@gmail.com>
2023-06-25 21:36:28 +0530

    updated

--- index.js
+++ index.js
@@ -3,40 +3,55 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
 const Movie = require('./Models/Movie');
 const cors = require('cors');
+const { MongoClient } = require('mongodb');
+const path = require('path');
+
 const app = express();
-const path=require("path");
 app.use(express.json());
-
-
 app.use(cors());
 
 const PORT = process.env.PORT || 3500;
-const DB_CONNECT = process.env.DB_CONNECT;
-app.use(express.static(path.join(__dirname,"/build")))
-app.get("*",(req,res)=>{
-  res.sendFile(path.join(__dirname,"./build/index.html"))
-});
+const uri = process.env.DB_CONNECT;
+
 // Connect to MongoDB
-mongoose
-  .connect(DB_CONNECT)
-  .then(() => {
+const connectToMongoDB = async () => {
+  try {
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    await client.connect();
     console.log('Database connected');
+    return client;
+  } catch (err) {
+    console.error('Failed to connect to MongoDB', err);
+    throw err;
+  }
+};
 
-   
-  })
-  .catch((err) => console.error(err));
+const startServer = async () => {
+  try {
+    // Establish MongoDB connection
+    const client = await connectToMongoDB();
 
+    // Perform any additional setup or configuration
 
-//router
-  
-const MovieRouter=require('./Routes/Movie')
+    // Serve static files
+    app.use(express.static(path.join(__dirname, '/build')));
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, './build/index.html'));
+    });
 
+    // Start the server
+    app.listen(PORT, () => {
+      console.log('Server connected on port', PORT);
+    });
+  } catch (error) {
+    console.error('Failed to start the server', error);
+    process.exit(1);
+  }
+};
+
+// Call the startServer function to initiate the server
+startServer();
+
+// Router
+const MovieRouter = require('./Routes/Movie');
 app.use('/', MovieRouter);
-
-
-
-
-
-app.listen(PORT, () => {
-    console.log('Server connected on port', PORT);
-  });
